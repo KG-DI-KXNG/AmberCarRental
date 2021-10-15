946da10b00a74fb03a1039f9e6038c4f1504f08d
Richard Wilson <richardmwilson191@gmail.com>
2021-10-15 12:55:01 -0500

    adding route files and connection

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+require __DIR__ . './richard.php';
+require __DIR__ . './akeem.php';
+require __DIR__ . './bailey.php';
+require __DIR__ . './ezra.php';
+require __DIR__ . './javoneil.php';
+require __DIR__ . './kevando.php';
+require __DIR__ . './latoya.php';
+require __DIR__ . './olivia.php';
+require __DIR__ . './ronald.php';
+require __DIR__ . './grant.php';
