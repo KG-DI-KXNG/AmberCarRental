473694cd05874b95d02c0e06aa600f687675795b
Richard Wilson <richardmwilson191@gmail.com>
2021-10-15 13:55:44 -0500

    Creating controllers

---
+++ app/Http/Controllers/Associates/AssociateController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Associates;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AssociateController extends Controller
+{
+    //
+}
