fcf7de825fcf92606008eae4baf6d04d5ddf35e7
OriginsPD <88017775+OriginsPD@users.noreply.github.com>
2021-10-15 14:22:34 -0500

    Routes Added For each Section is Folder

---
+++ app/Http/Controllers/Home/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function rental()
+    {
+        return;
+    }
+
+    public function plans()
+    {
+        return;
+    }
+
+    public function contact()
+    {
+        return;
+    }
+}
